d34d4447b9deb33b3f7d899036ea42dec5cce3ac
longgang <kevin__lg@163.com>
2020-07-12 14:55:54 +0800

    first commit

---
+++ po/scripts/test_login.py
@@ -0,0 +1,14 @@
+class LogIn :
+    def login1(self):
+        assert True
+    def login2(self):
+        assert True
+    def login3(self):
+        assert False
+    def login4(self):
+        assert False
+    def login5(self):
+        assert True
+    def login6(self):
+        assert True
+
