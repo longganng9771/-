b16131c7414d76f54fd3128e1a700b4b6509e85d
longgang <kevin__lg@163.com>
2020-07-12 15:16:23 +0800

    first commit

---
+++ 关于pytest/scripts/test_hello.py
@@ -0,0 +1,6 @@
+class TestHello:
+    def test_hello1(self):
+        print('hello1')
+    def test_hello2(self):
+        print('hello2')
+
---
+++ 关于pytest/scripts/test_login.py
@@ -0,0 +1,6 @@
+class TestLogin:
+    def test_login1(self):
+        print('login1')
+    def test_login2(self):
+        print('login2')
+
